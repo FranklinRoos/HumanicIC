e045734177ed9671bd6b6c8a24573520bd8e4094
Thijs <thijsvanhout276@gmail.com>
2016-08-12 01:34:18 +0200

    Laatste push verwijderd
    Push van Franklin verwijderd, is iets mis gegaan met de push. Werd niet
    in folder humanic gezet.

--- assets/js_B/fuik.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#rijbewijsCheck").change(function() {
-		alert("oke");
-		if ($("#rijbewijsCheck").checked) {
-			$("#auto").style.display = flex;
-		}
-	});
-
-});
