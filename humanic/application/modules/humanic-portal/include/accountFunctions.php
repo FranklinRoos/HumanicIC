0e2410905038e65f8cc4b7da6315a7ad9419489d
Frankie <Frankie@192.168.1.107>
2016-09-09 00:31:58 +0200

    Bart , de div waar je het over had heb ik niet kunnen vinden . De divs die nu in de header staan , horen er ook te staan. In de default_functions.php heb ik de functie fHeader aangepast, de in/uitlog knop heb ik overgeheveld naar de navigatie functie en die heb ik boven gefixeerd. Ik heb de functie handleAanmeldForm en verify.php aangepast, ik zag eerder op de avond dat er opeens wel hele vreemde dingen gebeurden bij de registratie procedure, die zijn nu volgens mij verholpen. En in de css , wat styling aangebracht voor de (registratie)meldingen Ik heb de registratie uitvoerig getest vannacht en geen onrgelmatigheden kunnen vaststellen. Thijs, ik ben benieuwd of je weer een afwijkende behandeling krijgt bij de registratie. In de database heb ik overigens niets aangepast.

--- humanic/application/modules/humanic-portal/include/accountFunctions.php
+++ humanic/application/modules/humanic-portal/include/accountFunctions.php
@@ -30,7 +30,7 @@ function handleModifyAccount ($naam,$email)
 {
     global  $connection;
             
-    if(md5(trim($_POST['modpasswd1']))==md5(trim($_POST['modpasswd2'])))
+    if(($_POST['modpasswd1'] != "" && $_POST['modpasswd2'] != "")&&(md5(trim($_POST['modpasswd1']))==md5(trim($_POST['modpasswd2']))))
     {
         $sql = mysqli_query($connection, "UPDATE `user` SET `user_wachtwoord`='".md5(trim($_POST['modpasswd1']))."' WHERE `user_inlognaam`= '".$_SESSION['loginnaam']."'");
         echo "".ucfirst($_SESSION["loginnaam"]).",je wachtwoord is gewijzigd<br>";    
--- humanic/application/modules/humanic-portal/register.php
+++ humanic/application/modules/humanic-portal/register.php
@@ -10,22 +10,6 @@ $pageNavId=6;
 fHeader($active=$pageNavId);
 navigatie($active=$pageNavId);
 
-// haal je gegevens voor de pagina uit database
-
-/*$sql = mysqli_query($connection, "SELECT * FROM `pages` where `page_nav_id`='2' and `page_show` ='y'");
-if (mysqli_num_rows($sql)==0)   
-{
-    die ("Je hebt geen gegevens tot je beschikking");
-}
-echo "<div class=\"container\">";
-
-while ($content = mysqli_fetch_assoc($sql)) 
-{
-    echo $content["page_title"];
-    
-    //echo "<h3>Registration Form</h3>";
-    
-}*/
 
 if(isSet($_SESSION['loginnaam'])) 
 {
